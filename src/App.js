7f2d7eba4b6e2250cdc14819bb2cef33b59ddabb
tobedoit <tobedoit@aol.com>
2023-03-09 20:15:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// https://www.youtube.com/watch?v=JSJ8ftr92Vw
+import Navbar from './scenes/Navbar';
+import DotGroup from './scenes/DotGroup';
+import Landing from './scenes/Landing';
+import MySkills from './scenes/MySkills';
+import Projects from './scenes/Projects';
+import Testimonials from './scenes/Testimonials';
+import Contact from './scenes/Contact';
+import Footer from './scenes/Footer';
+import { useState, useEffect } from 'react';
+import LineGradient from './components/LineGradient';
+import useMediaQurey from './hooks/useMediaQuery';
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQurey("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className='w-5/6 mx-auto md:h-full'>
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className='w-5/6 mx-auto md:h-full'>
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className='w-5/6 mx-auto'>
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className='w-5/6 mx-auto md:h-full'>
+        <Testimonials />
+      </div>
+      <LineGradient />
+      <div className='w-5/6 mx-auto md:h-full'>
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="w-6 h-6" alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="w-6 h-6" alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="w-6 h-6" alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="w-6 h-6" alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
